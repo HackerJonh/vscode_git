24fb8afe1261f9f6a33a01b620b5a7fad8323a2f
Juan Manuel <hackerjohn9559@gmail.com>
2020-11-22 13:35:37 -0400

    Validaciones

--- JS/validacion.js
+++ JS/validacion.js
@@ -33,7 +33,7 @@ function validaNombre() {
     var elemento = document.getElementById("nombre");
     if (elemento.value == "") {
         alert("El campo nombre no puede estar Vacio");
-        error(elemento);
+        Error(elemento);
         return false;
     }
     return true;
