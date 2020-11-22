6a3f535f8041df0c776dbe248323e808271c2f8c
Juan Manuel <hackerjohn9559@gmail.com>
2020-11-21 20:02:49 -0400

    ValidacionBasica

---
+++ JS/validacion.js
@@ -0,0 +1,83 @@
+//SELECCIONAR EL FORMULARIO
+
+//1-Conociendo el id
+
+var formulario = document.getElementById("miFormulario");
+//var formulario=document.forms["miFormulario"];
+
+//2-Conociendo el Numero de formulario que es en la Pagina
+
+//var formulario2 = document.getElementsByTagName("form")[0];
+//var formulario3 = document.forms[0];
+
+//SELECCIONAR ELEMENTOS DE UN FORMULARIO:
+
+//.elements[]: Devuelve un array con todos los inputs del formulario
+
+//getElementById(): devuelve un elemento con un id determinado
+
+//getElementsByTagName(): devuelve un array con elementos de tipo etiqueta
+
+//getElementsByName(): Devuelve un array con elementos que tienen el mismo nombre
+
+
+window.onload = iniciar;
+
+function iniciar() {
+    document.getElementById("enviar").addEventListener("click", validar, false);
+}
+
+//Validacion de elementos
+
+function validaNombre() {
+    var elemento = document.getElementById("nombre");
+    if (elemento.value == "") {
+        alert("El campo nombre no puede estar Vacio");
+        return false;
+    }
+    return true;
+}
+
+function validaTelefono() {
+    var elemento = document.getElementById("telefono");
+    if (isNaN(elemento.value)) {
+        alert("El campo telefono tiene que ser Numerico");
+        return false;
+    }
+    return true;
+}
+
+function validaFecha() {
+    var dia = document.getElementById("dia").value;
+    var mes = document.getElementById("mes").value;
+    var año = document.getElementById("año").value;
+
+    var fecha = new Date(año, mes, dia);
+    if (isNaN(fecha)) {
+        alert("Los Campos de fecha son incorrectos");
+        return false;
+
+    }
+    return true;
+}
+
+function validaCheck() {
+    var campoCheck = document.getElementById("mayor");
+    if (!campoCheck.checked) {
+        alert("Debes ser mayor de edad");
+        return false;
+
+    }
+    return true;
+}
+
+//Funcion para unir todas las Validaciones anteriormente le he nombrado validar
+
+function validar(e) {
+    if (validaNombre() && validaTelefono() && validaFecha() && validaCheck() && confirm("Pulsa Aceptar si deseas enviar el formulario")) {
+        return true
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
