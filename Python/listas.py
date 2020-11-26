3fa8a117342fd571efce6732f30dec958f1766c0
Juan Manuel <hackerjohn9559@gmail.com>
2020-11-26 19:24:42 -0400

    Cambios Nuevos

--- Python/listas.py
+++ Python/listas.py
@@ -1,1 +1,16 @@
 #LISTAS
+
+mascotas=["Perro", "Gato","Gallina","Caballo","Pato"]
+
+print(mascotas[:])
+print("La primera posicion de mi lista es: ",mascotas[0])
+print("La segunda Posicion de mi lista es: ",mascotas[1])
+print ("La tercera Posicion de mi lista esÇ:  ",mascotas[2])
+print("La cuarta Posicion de mi lista es:  ",mascotas[3])
+print("La ultima Posicion de mi lista es: ",mascotas[4])
+
+    
+
+
+
+
