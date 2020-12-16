a35546ba7d233e0b82ff52705656bcbee917ce8a
Juan Manuel <hackerjohn9559@gmail.com>
2020-12-16 14:47:14 -0400

    Python Execpiones

---
+++ Python/excepciones.py
@@ -0,0 +1,40 @@
+print("SISTEMA DE OPERACIONES MATEMATICAS")
+
+print("")
+
+def suma(num1,num2):
+    return num1+num2
+
+def resta(num1,num2):
+    return num1-num2
+
+def multiplica(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        print("No se puede divider entre cero")
+        return "Operacion erronea"
+
+    
+opt1=int(input("Introduce el primer Numero: "))
+
+opt2=int(input("Introduce el segundo numero: "))
+
+operacion=input("Introduce la operacion a Realizar (suma,resta,multiplica,divide): ")
+
+if operacion=="suma":
+    print(suma(opt1,opt2))
+    
+elif operacion=="resta":
+    print(suma(opt1,opt2))
+
+elif operacion=="multiplica":
+    print(multiplica(opt1,opt2))
+
+elif operacion=="divide":
+    print(divide(opt1,opt2))
+else:
+    print("Operacion no contemplada")
